5e5229c34158a4d2baa770f2022adf1e56fe3503
Zuev Anton <anton.zuev.1986@gmail.com>
2018-10-09 22:09:28 +0300

    исправлены ошибки

--- bot-w-calculator.py
+++ bot-w-calculator.py
@@ -88,3 +88,4 @@ def main():
     mybot.idle()
 
 main()
+
--- file.py
+++ file.py
@@ -2,13 +2,9 @@
 with open('referat.txt', 'r', encoding='utf-8') as f1:
     content = f1.read()
 print(len(content))
-sum_word = 0
-sum_word = 0
-with open('referat.txt', 'r', encoding='utf-8') as f2:
-    for line in f2:
-        if line != '\n':
-            sum_word += len(line.split(' '))
-print(sum_word)
+with open('referat.txt', 'r', encoding='utf-8') as f3:
+    content3 = f3.read()
+    print(len(content3.split()))
 new_content = content.replace('.', '!')
 with open('referat2.txt', 'w', encoding='utf-8') as f3:
     f3.write(new_content)
